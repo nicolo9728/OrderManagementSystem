aa6f807d37cf85935a96fdc579f1208f9dcd3331
nicolo9728 <nicolorossi8@gmail.com>
2026-07-05 17:33:22 +0200

    Add DeliveryServiceDbContext

---
+++ OrderManagementDeliveryService/Database/DeliveryServiceDbContext.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagementCommon;
+using OrderManagementCommon.Models;
+using OrderManagementDeliveryService.Models;
+
+namespace OrderManagementDeliveryService.Database;
+
+public class DeliveryServiceDbContext(IConfiguration configuration) : OrderManagementDbContext(configuration)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>((options) =>
+        {
+            options.HasKey((o) => o.Id);
+
+            options.Property((o) => o.Id)
+                .HasConversion((_) => _.Valore, (_) => new IdOrder(_));
+
+            options.Property((o) => o.IdProdotto)
+                .HasConversion((_) => _.Id, (_) => new IdProdotto(_));
+
+            options.Property((o) => o.IdUtente)
+                .HasConversion((_) => _.Id, (_) => new IdCustomer(_));
+
+            options.Property((o) => o.Quantita)
+                .HasConversion((_) => _.Valore, (_) => new Quantita(_));
+
+            options.Ignore((o) => o.Status);
+
+            options.ComplexProperty<OrderStatusRappresentation>("OrderStatusRappresentation", (options) =>
+            {
+                options.Property(p => p.MomentoCancellazione)
+                    .HasColumnName("MomentoCancellazione");
+
+                options.Property(p => p.MomentoConsegna)
+                    .HasColumnName("MomentoConsegna");
+
+                options.Property(p => p.RagioneCancellazione)
+                    .HasColumnName("RagioneCancellazione");
+
+                options.Property(p => p.IdDeliveryGuyAssegnato)
+                    .HasColumnName("IdDeliveryGuyAssegnato")
+                    .HasConversion<Guid?>((_)=>_ != null ? _.Id : null, (_)=> _ != null ? new IdDeliveryGuy(_.Value) : null);
+
+                options.Property(p => p.Tipo)
+                    .HasColumnName("Tipo");
+            })
+            .UsePropertyAccessMode(PropertyAccessMode.PreferProperty);
+        });
+    }
+}
