8daad5868f0a9a93227e82b9799986d46593b3b2
nicolo9728 <nicolorossi8@gmail.com>
2026-07-08 17:00:39 +0200

    Add row rappresentation of Event

---
+++ OrderManagementCommon/Events/DomainEventRow.cs
@@ -0,0 +1,35 @@
+using System.Text.Json;
+
+namespace OrderManagementCommon.Events;
+
+public record IdEvento(Guid Id);
+
+public class DomainEventRow
+{
+    public IdEvento Id { get; }
+    public string Tipo { get; }
+    public string Contenuto { get; }
+    public bool IsCompletato { get; private set; }
+    public DomainEventRow(string tipo, string contenuto)
+    {
+        Id = new IdEvento(Guid.NewGuid());
+        Tipo = tipo;
+        Contenuto = contenuto;
+    }
+
+    public void MarcaCompletato()
+        => IsCompletato = true;
+
+    private DomainEventRow()
+    {
+        Id = null!;
+        Tipo = null!;
+        Contenuto = null!;
+    }
+
+    public static DomainEventRow FromEvent(DomainEvent e)
+        => new(e.GetType().Name, JsonSerializer.Serialize(e, e.GetType()));
+
+    public DomainEvent? ToDomainEvent()
+        => JsonSerializer.Deserialize(Contenuto, typeof(DomainEvent).Assembly.GetType(Tipo)!) as DomainEvent;
+}
