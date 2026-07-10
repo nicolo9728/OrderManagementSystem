1059a3d2c8c35159866a9fb65f50c3581a14a37e
nicolo9728 <nicolorossi8@gmail.com>
2026-07-10 18:38:54 +0200

    Add OrderCreatoConsumer

---
+++ OrderManagementDeliveryService/Consumers/OrderCreatoConsumer.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagementCommon;
+using OrderManagementCommon.Events;
+using OrderManagementCommon.Models;
+using OrderManagementDeliveryService.Database;
+using OrderManagementDeliveryService.Models;
+
+namespace OrderManagementDeliveryService.Consumers;
+
+public class OrderCreatoConsumer(DeliveryServiceDbContext context) : IEventConsumer<OrderCreatoEvent>
+{
+
+    private async Task<DeliveryGuy> GetDeliveryGuyWithLessOrderAssigned()
+        => await context.DeliveryGuys
+            .OrderBy((d)=>d.NumeroConsegneAttive)
+            .Take(1)
+            .FirstAsync();
+
+    public async Task Consume(OrderCreatoEvent arg, CancellationToken cancellationToken)
+    {
+        Order? order = await context.Ordini.Where((o)=>o.Id == arg.Id).FirstOrDefaultAsync(cancellationToken);
+
+        if(order?.Status is not OrderEvaded)
+            return;
+
+        if(order == null)
+            return;
+        
+        using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
+        var deliveryGuyDaAssegnare = await GetDeliveryGuyWithLessOrderAssigned();
+
+        order.TryAssegna(deliveryGuyDaAssegnare.Id);
+
+        if(order.Status is OrderAssegnato)
+            deliveryGuyDaAssegnare.SegnalaNuovaConsegna();
+
+        await context.SaveChangesAsync(cancellationToken);
+        await transaction.CommitAsync(cancellationToken);
+    }
+}
