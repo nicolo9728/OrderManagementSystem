801e78d565fd5d26bfdf2673823eb5990d54497c
nicolo9728 <nicolorossi8@gmail.com>
2026-07-10 12:11:15 +0200

    Update Migrations

---
+++ OrderManagementDeliveryService/Migrations/20260710092102_Add momentoAcquisto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderManagementDeliveryService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddmomentoAcquisto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Ordini");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MomentoCreazione",
+                table: "Eventi",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MomentoAcquisto",
+                table: "Ordini",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ordini",
+                table: "Ordini",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordini_IdProdotto_IdUtente_MomentoAcquisto",
+                table: "Ordini",
+                columns: new[] { "IdProdotto", "IdUtente", "MomentoAcquisto" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ordini",
+                table: "Ordini");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ordini_IdProdotto_IdUtente_MomentoAcquisto",
+                table: "Ordini");
+
+            migrationBuilder.DropColumn(
+                name: "MomentoCreazione",
+                table: "Eventi");
+
+            migrationBuilder.DropColumn(
+                name: "MomentoAcquisto",
+                table: "Ordini");
+
+            migrationBuilder.RenameTable(
+                name: "Ordini",
+                newName: "Order");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "Id");
+        }
+    }
+}
--- OrderManagementUserService/Migrations/20260710093105_Add Momento creazione.Designer.cs
+++ OrderManagementUserService/Migrations/20260710093105_Add Momento creazione.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OrderManagementUserService.Database;
@@ -12,9 +13,11 @@ using OrderManagementUserService.Database;
 namespace OrderManagementUserService.Migrations
 {
     [DbContext(typeof(UserServiceDbContext))]
-    partial class UserServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260710093105_Add Momento creazione")]
+    partial class AddMomentocreazione
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
